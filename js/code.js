80fd63d6291b0835bbf1185e488e29aad9d131d9
MrJop <jop@interactionfigure.nl>
2025-03-20 11:35:56 +0100

    test code completed

--- js/code.js
+++ js/code.js
@@ -1,18 +1,65 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    PusherManager.init();
-    PusherManager.connectToChannel();
-
-    /////
-
-    document.getElementById('send-button').onclick = function() {
-        PusherManager.sendMessageToChannel({
-            msg: document.getElementById('message-to-send').value,
-            gameID: 123
-        });
-
-        document.getElementById('message-to-send').value = '';
-    }
+    GameConnector.init();
 });
+
+
+var GameConnector = {
+    GAME_ENTRY_CODE: 'jop',
+    GAME_ID: "my game name",
+
+    init: function() {
+        PusherManager.init();
+        PusherManager.connectToChannel();
+    },
+    
+    onPusherConnected: function() {
+        this.showCodeScreen();
+    },
+
+    showCodeScreen: function() {
+        document.getElementById('code').classList.add('--show');
+
+        document.getElementById('code-submit').onclick = function() {
+            GameConnector.onCodeSubmitClicked();
+        }
+    },
+
+    onCodeSubmitClicked: function() {
+        if(document.getElementById('code-input').value == this.GAME_ENTRY_CODE){
+            this.showGameScreen();
+        }else{
+            document.getElementById('code-input').value = '';
+        }
+    },
+
+    showGameScreen: function() {
+        document.getElementById('code').classList.remove('--show');
+        document.getElementById('game').classList.add('--show');
+        //
+        document.getElementById('test-finish').onclick = function() {
+            GameConnector.onGameFinishClicked();
+        }
+    },
+
+    onGameFinishClicked: function () {
+        //Send message to server
+        PusherManager.sendMessageToChannel({
+            msg: 'Game Finished!',
+            gameID: this.GAME_ID
+        });
+        //
+        document.getElementById('game').classList.remove('--show');
+        document.getElementById('result').classList.add('--show');
+    }
+};
+
+
+
+
+
+
+
+
 
 var PusherManager = {
     CHANNEL_ID: "blockbuster",
@@ -35,15 +82,14 @@ var PusherManager = {
 
     connectToChannel: function () {
         this.presenceChannel = this.pusher.subscribe('presence-'+this.CHANNEL_ID);
-        this.presenceChannel.bind('pusher:subscription_succeeded', this.onSubScriptionSucceeded.bind(this));
+        this.presenceChannel.bind('pusher:subscription_succeeded', this.onSubscriptionSucceeded.bind(this));
     },
 
-    onSubScriptionSucceeded: function (_data) {
-        document.getElementById('demo').style.display = 'block';
+    onSubscriptionSucceeded: function (_data) {
         this.sUserID = _data.myID+"";
 
-        console.log('onSubScriptionSucceeded', _data);
-
+        GameConnector.onPusherConnected()
+        
         this.presenceChannel.bind('pusher:member_added', this.onMemberAdded.bind(this));
         this.presenceChannel.bind('pusher:member_removed', this.onMemberRemoved.bind(this));
         this.presenceChannel.bind('client-messagetochannel', this.onMessageFromOtherPlayer.bind(this));
